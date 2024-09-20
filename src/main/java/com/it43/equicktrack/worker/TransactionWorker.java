6bc43cca8d98e9d5fb63dbb8ceac506d4a4a2ba3
Mortreddd <emmanmale@gmail.com>
2024-09-20 15:24:54 +0800

    Fixed transaction worker for the late returnees

--- src/main/java/com/it43/equicktrack/worker/TransactionWorker.java
+++ src/main/java/com/it43/equicktrack/worker/TransactionWorker.java
@@ -1,6 +1,7 @@
 package com.it43.equicktrack.worker;
 
 
+import com.it43.equicktrack.dto.transaction.TransactionDTO;
 import com.it43.equicktrack.transaction.Transaction;
 import com.it43.equicktrack.transaction.TransactionService;
 import com.it43.equicktrack.util.Constant;
@@ -25,11 +26,11 @@ public class TransactionWorker {
     @Scheduled(fixedRate = TEST_TIME_CHECK)
 //    @Scheduled(fixedRate = Constant.TIME_CHECK)
     public void checkLateReturnEquipments(){
-        List<Transaction> lateReturnees = transactionService.getTransactions()
+        List<TransactionDTO> lateReturnees = transactionService.getTransactions()
                 .stream()
-                .filter(transaction ->
-                        DateUtilities.isLate(transaction.getReturnDate()) && transaction.getReturnedAt() != null
-                )
+                .filter(transaction -> {
+                        return DateUtilities.isLate(transaction.returnDate()) && transaction.returnedAt() == null;
+                })
                 .toList();
 
         log.info("Executed at {}", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
