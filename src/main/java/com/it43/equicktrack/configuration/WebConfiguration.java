2fc274ec5a708038dfcd7e3a9e4f9170ead5ef6f
Mortreddd <emmanmale@gmail.com>
2024-10-02 10:38:49 +0800

    Fixed cors for deployment

--- src/main/java/com/it43/equicktrack/configuration/WebConfiguration.java
+++ src/main/java/com/it43/equicktrack/configuration/WebConfiguration.java
@@ -22,7 +22,7 @@ public class WebConfiguration {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/v1/**")
-                        .allowedOrigins("https://wily-rena-equicktrack-4bc9ec67.koyeb.app", frontendUrl)
+                        .allowedOrigins(frontendUrl)
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowCredentials(true);
