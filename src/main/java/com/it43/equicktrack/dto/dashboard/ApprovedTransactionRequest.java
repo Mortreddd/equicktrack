1b701dfc80abbcf7f6b62ad591eb07e7ce86aab5
Mortreddd <emmanmale@gmail.com>
2024-11-27 08:41:40 +0800

    Fix the reviews of return equipment

--- src/main/java/com/it43/equicktrack/dto/dashboard/ApprovedTransactionRequest.java
+++ src/main/java/com/it43/equicktrack/dto/dashboard/ApprovedTransactionRequest.java
@@ -1,6 +1,7 @@
 package com.it43.equicktrack.dto.dashboard;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.it43.equicktrack.equipment.Remark;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,5 +14,7 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ApprovedTransactionRequest {
 
+    private Boolean available;
+    private Remark remark;
     private String message;
 }
